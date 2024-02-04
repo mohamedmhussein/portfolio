278731d04f9c4d178689c5f76be6bbf972fe6804
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-02-03 20:03:32 -0500

    Finalizes the hero section:

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Link from "next/link"
 import "./main.scss"
 import { Navbar } from "@/components/navbar/Navbar"
+import Hero from "@/components/hero/Hero"
 
 export default function Home() {
   return (
     <div>
-      <section id="Home"><Navbar /></section>
+      <section id="Home">
+        <Navbar />
+        <Hero />
+      </section>
       <section id="Services">parallex</section>
       <section>Services</section>
       <section id="Portfolio">parallex</section>
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,66 @@
+'use client'
+import './hero.scss'
+import {motion} from 'framer-motion'
+
+const Hero = () => {
+
+  const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0
+    },
+    animate: {
+        x:0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    },
+    scrollButton: {
+        opacity: 0,
+        y: 10,
+        transition: {
+            duration:2,
+            repeat: Infinity,
+        }
+    }
+  }  
+  const sliderVariants = {
+    initial: {
+        x:0
+    },
+    animate: {
+        x: '-250%',
+        transition: {
+            repeat: Infinity,
+            duration: 20,
+            repeatType: 'mirror'
+        }
+    }
+
+  }
+  return (
+    <div className='hero'>
+        <div className="wrapper">
+            <motion.div className="textContainer" variants={textVariants} initial='initial' animate='animate'>
+                <motion.h2 variants={textVariants}>MOHAMMAD EL PRINCE</motion.h2>
+                <motion.h1 variants={textVariants}>Full-Stack <br />Web Developer</motion.h1>
+                <motion.div className="buttons" variants={textVariants}>
+                    <motion.button variants={textVariants}>See My Work</motion.button>
+                    <motion.button variants={textVariants}>Contact Me</motion.button>
+                </motion.div>
+                <motion.img variants={textVariants} src="/scroll.png" alt="" animate='scrollButton' />
+            </motion.div>
+        </div>
+        <motion.div className="slidingTextContainer" variants={sliderVariants} initial='initial' animate='animate'>
+            JavaScript, Python, React, Flask, Nextjs, SQLite
+        </motion.div>
+        <div className="imageContainer">
+            <img src="/myimage.png" alt="myimage" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
