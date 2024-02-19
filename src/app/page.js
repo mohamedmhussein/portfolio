20e6ec670627bc5fc413a5dcf7fc8ae1cd46c7e2
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-02-19 13:14:52 -0500

    Applies the parallax animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-import Link from "next/link"
-import "./main.scss"
+import "./page.scss"
 import { Navbar } from "@/components/navbar/Navbar"
 import Hero from "@/components/hero/Hero"
 import Parallax from "@/components/parallax/Parallax"
@@ -7,19 +6,22 @@ import Parallax from "@/components/parallax/Parallax"
 export default function Home() {
   return (
     <div>
+
       <section id="Home">
         <Navbar />
         <Hero />
       </section>
+
       <section>
         <Parallax  title='services'/>
       </section>
       
       <section>Services</section>
-      <Parallax />
+
       <section>
         <Parallax  title='portfolio'/>
       </section>
+      
       <section>portfolio2</section>
       <section>portfolio3</section>
       <section id="Contact">Contat</section>
--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,15 +1,37 @@
+'use client'
+import { useRef } from 'react'
 import './parallax.scss'
+import {motion, useScroll, useTransform} from 'framer-motion'
+import { urlToUrlWithoutFlightMarker } from 'next/dist/client/components/app-router'
 const Parallax = ({title}) => {
+
+  const ref= useRef()
+  
+  const {scrollYProgress} = useScroll({
+    target: ref,
+    offset: ['start start', 'end start'],
+  })
+
+  const yAnimation = useTransform(scrollYProgress, [0,1], ["0%", "100%"])
+  const yText = useTransform(scrollYProgress, [0,1], ["0%", "500%"])
+
+
+
   return (
-    <div className='parallax' style = {{
+    <div ref = {ref}className='parallax' style = {{
         background: 
             title === 'services' 
                 ? "linear-gradient(180deg, #111132, #0c0c1d)" 
                 : "linear-gradient(180deg, #111132, #505064)" }}>
-        <h1>{ title === 'services'? 'What I do?' : 'What I did?'}</h1>
-        <div className="mountains"></div>
-        <div className="planets"></div>
-        <div className="stars"></div>
+        <motion.h1 style={{y: yText}}>{ title === 'services'? 'What I do?' : 'What I did?'}</motion.h1>
+        <motion.div  className="mountains"></motion.div>
+        <motion.div 
+        style = {{
+          y: yAnimation,
+          backgroundImage: `url(${title === 'services' ? '/planets.png': '/sun.png'})`
+        }} 
+        className="planets"></motion.div>
+        <motion.div style = {{x: yAnimation}} className="stars"></motion.div>
     </div>
   )
 }
